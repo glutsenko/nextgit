c08e9ba37fed8cdf3cb48abdcdadd118d11678d9
glutsenko <grigoriylutsenko@gmail.com>
2019-01-14 22:51:42 +0200

    change to notepad

--- index.php
+++ index.php
@@ -8,7 +8,50 @@
     <title>DVLPR web</title>
 </head>
 <body>
-    <header>123</header>
-    <footer></footer>
+<header>Header</header>
+<div class="container">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae consectetur cupiditate delectus distinctio dolores
+    esse et illo laborum minima nostrum officiis porro quasi ratione sapiente suscipit, ullam, voluptatum. Deserunt
+    dicta exercitationem rerum veritatis voluptatem! Dolor ducimus et, maxime omnis perspiciatis placeat possimus sit!
+    Ad blanditiis consequatur cumque, delectus deleniti esse harum, inventore, odit perspiciatis repellat sequi soluta
+    tempore tenetur? Blanditiis ducimus, ea illo laboriosam magni molestiae numquam odio officiis porro? A cupiditate
+    doloribus natus neque nulla quibusdam totam unde, voluptatem. Accusantium aut beatae consequuntur cupiditate dolore
+    dolores expedita hic id ipsam iste magnam minus, modi molestias nostrum odio reiciendis sed totam voluptatum? Amet
+    blanditiis ducimus eos harum illum nesciunt quam repellat sapiente voluptate! Culpa, doloribus in quam reiciendis
+    similique voluptates. Culpa cupiditate doloribus dolorum facilis impedit ipsa molestiae optio quidem? Cum et impedit
+    iure nihil non rem sunt. Aliquid magni nobis odio rerum voluptates. A architecto aut autem commodi corporis, culpa
+    dicta distinctio dolor doloremque dolores esse facilis, incidunt inventore, ipsam iste iusto modi necessitatibus
+    neque nihil nobis nulla officia pariatur perspiciatis placeat provident quasi quisquam repellendus saepe sit
+    temporibus ullam unde veritatis voluptatibus! Aperiam laboriosam porro similique tempore tenetur! Accusantium
+    aliquam amet autem corporis dicta doloremque ducimus ea eius et, exercitationem facilis in laboriosam molestiae,
+    nisi numquam officia quae quis quod saepe sequi tempore unde veniam voluptatem voluptates, voluptatibus! Aperiam
+    dignissimos, distinctio dolorem eos error exercitationem fugit itaque, molestiae molestias nihil non reprehenderit,
+    soluta voluptatibus. Aliquid cum delectus, deserunt distinctio, ea earum eligendi explicabo facilis fuga fugit hic
+    itaque laborum magni maxime molestiae, mollitia natus omnis quae quaerat quis reiciendis repellendus saepe sapiente
+    tempore totam ullam veritatis. Atque dicta enim et incidunt, minus, odit officiis perferendis praesentium quas quasi
+    sint ullam. Ab alias aliquam aperiam aut culpa dicta dolor dolore dolores, earum enim eos fuga hic id impedit iste
+    minima molestiae mollitia numquam odit recusandae repellendus reprehenderit repudiandae saepe similique, suscipit,
+    temporibus veniam vero voluptas voluptate voluptates! Commodi maxime nemo vero? Aliquid aperiam autem cumque
+    deserunt earum esse, hic impedit natus non nostrum pariatur placeat quod repellendus sit soluta. Aspernatur cum
+    repellendus tempore temporibus veritatis. Accusamus ad animi aspernatur, assumenda beatae dolore facere, quae qui
+    quidem quis sed, tempora? Animi assumenda doloremque ipsum modi nobis nostrum perspiciatis quis totam, voluptate.
+    Harum, laudantium, tempore! Animi ipsam mollitia nulla odit quas. A alias aspernatur corporis cumque deserunt esse
+    iusto. A aspernatur eveniet exercitationem harum, hic incidunt magni nesciunt, nisi nobis nostrum nulla officia quis
+    tempora. Accusamus alias dicta enim inventore, ipsam, magni molestiae molestias quae quam quod sequi similique velit
+    vitae? Debitis eos ipsam, iusto labore molestiae nesciunt non quibusdam repellendus reprehenderit sapiente sequi sit
+    veniam. Ab accusamus aliquam dolorum enim, error ipsam laudantium minima officiis quod rem sint soluta vitae
+    voluptatum? Accusamus alias architecto, atque aut deleniti distinctio dolor dolorem doloremque ea exercitationem
+    facilis fugit id iste labore libero maxime natus nisi non nostrum numquam obcaecati odit optio possimus quibusdam
+    quod reiciendis reprehenderit sunt tenetur velit vero! Ducimus, harum, officia? Atque dolorem ea nam numquam
+    repudiandae saepe veniam. Magnam, ullam.
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aperiam corporis eius excepturi ipsa magni,
+        molestias perferendis saepe temporibus, ut vel voluptatum? Aliquid architecto dolorem eaque illo officiis
+        perferendis recusandae vero vitae. Corporis cupiditate debitis dicta dignissimos eius eum fugit minima neque non
+        officiis pariatur praesentium quisquam, sed similique ullam, veniam voluptate! Atque est explicabo illo nulla
+        rem? Aliquam amet cumque deleniti dolore eos laborum minima neque porro qui unde. Accusamus, adipisci, amet aut
+        beatae cumque cupiditate dolorem eaque earum eos ex excepturi exercitationem fugiat illum minus necessitatibus,
+        neque quae quasi reiciendis rerum suscipit temporibus ullam ut vel voluptas voluptatem?</p>
+</div>
+<footer>Footer</footer>
 </body>
 </html>
