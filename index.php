0e4ee0022e67a0a805b58b336e9473bf989510d0
glutsenko <grigoriylutsenko@gmail.com>
2019-01-20 18:03:27 +0200

    1212

--- index.php
+++ index.php
@@ -51,6 +51,9 @@
         rem? Aliquam amet cumque deleniti dolore eos laborum minima neque porro qui unde. Accusamus, adipisci, amet aut
         beatae cumque cupiditate dolorem eaque earum eos ex excepturi exercitationem fugiat illum minus necessitatibus,
         neque quae quasi reiciendis rerum suscipit temporibus ullam ut vel voluptas voluptatem?</p>
+
+        <p>necessitatibus,
+        neque quae quasi reiciendis rerum suscipit temporibus ullam ut vel voluptas voluptatem?</p>
 </div>
 <footer>Footer</footer>
 </body>
